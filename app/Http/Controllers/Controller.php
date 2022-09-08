86a7ee4ab1704b50fbb7ef7cc1e1dab717d74aba
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-09-08 17:44:51 -0400

    feat: add responseOk and injectRepository methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\BaseRepository;
+use App\Repositories\CarRepository;
+use App\Repositories\TruckRepository;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +13,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /* Return response */
+    public function responseOk($response)
+    {
+        return response()->json([
+            'data' => $response
+        ]);
+    }
+
+    /* Inject repository on demand */
+    public function injectRepository($type = null)
+    {
+        if ($type === null) $this->repository = new BaseRepository();
+        if ($type === 'car') $this->repository = new CarRepository();
+        if ($type === 'truck') $this->repository = new TruckRepository();
+    }
 }
